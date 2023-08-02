502c00d27005677a89621c74f4cc69e23e736210
Yan Tong <ytong24@ucsc.edu>
2023-08-01 18:57:39 -0700

    add inference logic into infersentence.py

--- storm/multilang/resources/infersentence.py
+++ storm/multilang/resources/infersentence.py
@@ -1,19 +1,21 @@
-# from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import storm
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+
+print(tokenizer.decode(outputs[0]))
 
 class SplitSentenceBolt(storm.BasicBolt):
     def process(self, tup):
-        testSentence = tup.values[0]
-        expectedWords = tup.values[1]
-        storm.emit([expectedWords, expectedWords])
+        testSentence = str(tup.values[0])
+        expectedWords = str(tup.values[1])
+
+        inputs = tokenizer.encode(testSentence, return_tensors="pt")
+        outputs = model.generate(inputs, max_length=3, do_sample=True)
+        createdWords = str(tokenizer.decode(outputs))
+        storm.emit([expectedWords, createdWords])
 
 SplitSentenceBolt().run()
 
-# tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-# model = GPT2LMHeadModel.from_pretrained("gpt2")
 
-# inputs = tokenizer.encode("I enjoy walking with my cute", return_tensors="pt")
-# outputs = model.generate(inputs, max_length=10, do_sample=True)
-
-# print(tokenizer.decode(outputs[0]))
